ff8b2e0b49b4e9721bc07b30587dd26629856670
Noa Houssier <nestornebula.dev@gmail.com>
2026-03-31 14:04:53 +0200

    test: update tests following prototypes changes

--- test/texture_from_line.c
+++ test/texture_from_line.c
@@ -21,7 +21,7 @@ int		create_test_file(char *name, char *content);
 int	main(void)
 {
 	t_unit_test	*unit_test;
-	t_textures	textures;
+	t_scene		scene;
 
 	if (create_test_file(NO_PATH, "") == -1
 		|| create_test_file(SO_PATH, "") == -1
@@ -29,54 +29,54 @@ int	main(void)
 		|| create_test_file(EA_PATH, ""))
 		return (0);
 	unit_test = new_unit_test("texture_from_line", false);
-	cut_expect(texture_from_line("NO " NO_PATH, &textures) != 0,
+	cut_expect(texture_from_line("NO " NO_PATH, &scene) != 0,
 			unit_test, "returns 1 after setting NO texture");
-	cut_expect(strcmp(textures.no.path, NO_PATH) == 0,
+	cut_expect(strcmp(scene.textures.no.path, NO_PATH) == 0,
 			unit_test, "stores correct path for NO texture");
-	cut_expect(texture_from_line("SO " SO_PATH, &textures) != 0,
+	cut_expect(texture_from_line("SO " SO_PATH, &scene) != 0,
 			unit_test, "returns 1 after setting SO texture");
-	cut_expect(strcmp(textures.so.path, SO_PATH) == 0,
+	cut_expect(strcmp(scene.textures.so.path, SO_PATH) == 0,
 			unit_test, "stores correct path for SO texture");
-	cut_expect(texture_from_line("WE " WE_PATH, &textures) != 0,
+	cut_expect(texture_from_line("WE " WE_PATH, &scene) != 0,
 			unit_test, "returns 1 after setting WE texture");
-	cut_expect(strcmp(textures.we.path, WE_PATH) == 0,
+	cut_expect(strcmp(scene.textures.we.path, WE_PATH) == 0,
 			unit_test, "stores correct path for WE texture");
-	cut_expect(texture_from_line("EA " EA_PATH, &textures) != 0,
+	cut_expect(texture_from_line("EA " EA_PATH, &scene) != 0,
 			unit_test, "returns 1 after setting EA texture");
-	cut_expect(strcmp(textures.ea.path, EA_PATH) == 0,
+	cut_expect(strcmp(scene.textures.ea.path, EA_PATH) == 0,
 			unit_test, "stores correct path for EA texture");
-	cut_expect(texture_from_line("F " F_R "," F_G "," F_B, &textures) != 0,
+	cut_expect(texture_from_line("F " F_R "," F_G "," F_B, &scene) != 0,
 			unit_test, "returns 1 after setting F color");
-	cut_expect((textures.f >> 16 & 255) == atoi(F_R),
+	cut_expect((scene.textures.f >> 16 & 255) == atoi(F_R),
 			unit_test, "sets F's red value correctly");
-	cut_expect((textures.f >> 8 & 255) == atoi(F_G),
+	cut_expect((scene.textures.f >> 8 & 255) == atoi(F_G),
 			unit_test, "sets F's green value correctly");
-	cut_expect((textures.f & 255) == atoi(F_B),
+	cut_expect((scene.textures.f & 255) == atoi(F_B),
 			unit_test, "sets F's blue value correctly");
-	cut_expect(texture_from_line("C " C_R "," C_G "," C_B, &textures) != 0,
+	cut_expect(texture_from_line("C " C_R "," C_G "," C_B, &scene) != 0,
 			unit_test, "returns 1 after setting C color");
-	cut_expect((textures.c >> 16 & 255) == atoi(C_R),
+	cut_expect((scene.textures.c >> 16 & 255) == atoi(C_R),
 			unit_test, "sets C's red value correctly");
-	cut_expect((textures.c >> 8 & 255) == atoi(C_G),
+	cut_expect((scene.textures.c >> 8 & 255) == atoi(C_G),
 			unit_test, "sets C's green value correctly");
-	cut_expect((textures.c & 255) == atoi(C_B),
+	cut_expect((scene.textures.c & 255) == atoi(C_B),
 			unit_test, "sets C's blue value correctly");
-	free(textures.no.path);
-	free(textures.no.path);
-	free(textures.no.path);
-	free(textures.no.path);
+	free(scene.textures.no.path);
+	free(scene.textures.no.path);
+	free(scene.textures.no.path);
+	free(scene.textures.no.path);
 
-	cut_expect(texture_from_line("A" " NA", &textures) == 0,
+	cut_expect(texture_from_line("A" " NA", &scene) == 0,
 			unit_test, "returns 0 for invalid identifier");
-	cut_expect(texture_from_line("NO " "no_such_file", &textures) == 0,
+	cut_expect(texture_from_line("NO " "no_such_file", &scene) == 0,
 			unit_test, "returns 0 for invalid filepath");
-	cut_expect(texture_from_line("F" "255,255,256", &textures) == 0,
+	cut_expect(texture_from_line("F" "255,255,256", &scene) == 0,
 			unit_test, "returns 0 for invalid color (overflow)");
-	cut_expect(texture_from_line("F" "255,-1,255", &textures) == 0,
+	cut_expect(texture_from_line("F" "255,-1,255", &scene) == 0,
 			unit_test, "returns 0 for invalid color (negative)");
-	cut_expect(texture_from_line("F" "255,255,255,255", &textures) == 0,
+	cut_expect(texture_from_line("F" "255,255,255,255", &scene) == 0,
 			unit_test, "returns 0 for invalid color (longer than RGB format)");
-	cut_expect(texture_from_line("F" "255,255", &textures) == 0,
+	cut_expect(texture_from_line("F" "255,255", &scene) == 0,
 			unit_test, "returns 0 for invalid color (shorter than RGB format)");
 
 	unlink(NO_PATH);
