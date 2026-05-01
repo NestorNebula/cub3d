eb9601244da17c2372c150261b9de3d65afd2575
Noa Houssier <nestornebula.dev@gmail.com>
2026-05-01 14:44:13 +0200

    feat: allow multiple directions move
    Define different speeds for movement and rotation

--- src/window/move.c
+++ src/window/move.c
@@ -17,26 +17,25 @@ static void	move_top(t_data *data, t_map *map, double speed);
 
 static void move_down(t_data *data, t_map *map, double speed);
 
-static void	move_left(int lookflag, t_data *data, t_map *map, double speed);
+static void	move_left(int moveflag, t_data *data, double mspeed, double rspeed);
 
-static void move_right(int lookflag, t_data *data, t_map *map, double speed);
+static void move_right(int moveflag, t_data *data, double mspeed, double rspeed);
 
-void		move(t_dir dir, int lookflag, t_data *data)
+void		move(int moveflag, t_data *data)
 {
-	double	speed;
+	double	move_speed;
+	double	rot_speed;
 
-	if (lookflag)
-		speed = data->frame_time * 5.0;
-	else
-		speed = data->frame_time * 3.0;
-	if (dir == D_UP)
-		move_top(data, &data->scene->map, speed);
-	else if (dir == D_DOWN)
-		move_down(data, &data->scene->map, speed);
-	else if (dir == D_LEFT || dir == D_LEFTL)
-		move_left(lookflag, data, &data->scene->map, speed);
-	else if (dir == D_RIGHT || dir == D_RIGHTL)
-		move_right(lookflag, data, &data->scene->map, speed);
+	move_speed = data->frame_time * 5.0;
+	rot_speed = data->frame_time * 3.0;
+	if (moveflag & D_UP)
+		move_top(data, &data->scene->map, move_speed);
+	if (moveflag & D_DOWN)
+		move_down(data, &data->scene->map, move_speed);
+	if ((moveflag & D_LEFT) || (moveflag & D_LEFTL))
+		move_left(moveflag, data, move_speed, rot_speed);
+	if ((moveflag & D_RIGHT) || (moveflag & D_RIGHTL))
+		move_right(moveflag, data, move_speed, rot_speed);
 }
 
 static void	move_top(t_data *data, t_map *map, double speed)
@@ -56,50 +55,54 @@ static void move_down(t_data *data, t_map *map, double speed)
 		data->player.pos_y -= data->player.dir_y * speed;
 }
 
-static void	move_left(int lookflag, t_data *data, t_map *map, double speed)
+static void	move_left(int moveflag, t_data *data, double mspeed, double rspeed)
 {
-	const t_player old_player = data->player;
+	const t_player	old_player = data->player;
+	t_map			*map;
 
-	if (lookflag)
+	if (moveflag & D_LEFTL)
 	{
-		data->player.dir_x = data->player.dir_x * cos(-speed)
-			- data->player.dir_y * sin(-speed);
-		data->player.dir_y = old_player.dir_x * sin(-speed)
-			+ data->player.dir_y * cos(-speed);
-		data->player.plane_x = data->player.plane_x * cos(-speed)
-			- data->player.plane_y * sin(-speed);
-		data->player.plane_y = old_player.plane_x * sin(-speed)
-			+ data->player.plane_y * cos(-speed);
+		data->player.dir_x = data->player.dir_x * cos(-rspeed)
+			- data->player.dir_y * sin(-rspeed);
+		data->player.dir_y = old_player.dir_x * sin(-rspeed)
+			+ data->player.dir_y * cos(-rspeed);
+		data->player.plane_x = data->player.plane_x * cos(-rspeed)
+			- data->player.plane_y * sin(-rspeed);
+		data->player.plane_y = old_player.plane_x * sin(-rspeed)
+			+ data->player.plane_y * cos(-rspeed);
 	}
-	else
+	map = &data->scene->map;
+	if (moveflag & D_LEFT)
 	{
-		if (is_walkable(map, data->player.pos_x - data->player.plane_x * speed, data->player.pos_y))
-			data->player.pos_x -= data->player.plane_x * speed;
-		if (is_walkable(map, data->player.pos_x, data->player.pos_y - data->player.plane_y * speed))
-			data->player.pos_y -= data->player.plane_y * speed;
+		if (is_walkable(map, data->player.pos_x - data->player.plane_x * mspeed, data->player.pos_y))
+			data->player.pos_x -= data->player.plane_x * mspeed;
+		if (is_walkable(map, data->player.pos_x, data->player.pos_y - data->player.plane_y * mspeed))
+			data->player.pos_y -= data->player.plane_y * mspeed;
 	}
 }
 
-static void move_right(int lookflag, t_data *data, t_map *map, double speed)
+static void move_right(int moveflag, t_data *data, double mspeed, double rspeed)
 {
-	const t_player old_player = data->player;
+	const t_player	old_player = data->player;
+	t_map			*map;
 
-	if (lookflag)
+	if (moveflag & D_RIGHTL)
 	{
-		data->player.dir_x = data->player.dir_x * cos(speed)
-			- data->player.dir_y * sin(speed);
-		data->player.dir_y = old_player.dir_x * sin(speed)
-			+ data->player.dir_y * cos(speed);
-		data->player.plane_x = data->player.plane_x * cos(speed)
-			- data->player.plane_y * sin(speed);
-		data->player.plane_y = old_player.plane_x * sin(speed)
-			+ data->player.plane_y * cos(speed);
+		data->player.dir_x = data->player.dir_x * cos(rspeed)
+			- data->player.dir_y * sin(rspeed);
+		data->player.dir_y = old_player.dir_x * sin(rspeed)
+			+ data->player.dir_y * cos(rspeed);
+		data->player.plane_x = data->player.plane_x * cos(rspeed)
+			- data->player.plane_y * sin(rspeed);
+		data->player.plane_y = old_player.plane_x * sin(rspeed)
+			+ data->player.plane_y * cos(rspeed);
 	}
-	else
+	map = &data->scene->map;
+	if (moveflag & D_RIGHT)
 	{
-		if (is_walkable(map, data->player.pos_x + data->player.plane_x * speed, data->player.pos_y))
-			data->player.pos_x += data->player.plane_x * speed;
-		if (is_walkable(map, data->player.pos_x, data->player.pos_y + data->player.plane_y * speed))
-			data->player.pos_y += data->player.plane_y * speed;
+		if (is_walkable(map, data->player.pos_x + data->player.plane_x * mspeed, data->player.pos_y))
+			data->player.pos_x += data->player.plane_x * mspeed;
+		if (is_walkable(map, data->player.pos_x, data->player.pos_y + data->player.plane_y * mspeed))
+			data->player.pos_y += data->player.plane_y * mspeed;
 	}
 }
