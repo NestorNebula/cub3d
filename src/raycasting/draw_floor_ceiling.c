823292da201a457cc6990b0c856ae6960f1193d5
Noa Houssier <nestornebula.dev@gmail.com>
2026-05-02 09:47:02 +0200

    feat: update draw_floor_ceiling function
    Use shading functions to give shade effect

--- src/raycasting/draw_floor_ceiling.c
+++ src/raycasting/draw_floor_ceiling.c
@@ -6,31 +6,27 @@
 /*   By: cmonmire <cmonmire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 08:46:47 by cmonmire          #+#    #+#             */
-/*   Updated: 2026/04/21 08:54:22 by cmonmire         ###   ########.fr       */
+/*   Updated: 2026/05/02 09:41:03 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core.h"
 
-void	draw_floor_ceiling(t_data *data)
+void draw_floor_ceiling(t_data *data, t_draw *draw, t_ray *ray)
 {
-	int	x;
 	int	y;
-	int	mid;
 
-	mid = data->screen_height / 2;
+	(void) ray;
 	y = 0;
+	while (y < draw->start)
+	{
+		my_mlx_pixel_put(data, draw->x, y, shade_ceiling(data->ceiling_color, y, data));
+		y++;
+	}
+	y = draw->end;
 	while (y < data->screen_height)
 	{
-		x = 0;
-		while (x < data->screen_width)
-		{
-			if (y < mid)
-				my_mlx_pixel_put(data, x, y, data->ceiling_color);
-			else
-				my_mlx_pixel_put(data, x, y, data->floor_color);
-			x++;
-		}
+		my_mlx_pixel_put(data, draw->x, y, shade_floor(data->floor_color, y, data));
 		y++;
 	}
 }
