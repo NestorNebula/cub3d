35832f1ff160529ae49fd307cc25397353fd9e9d
Noa Houssier <nestornebula.dev@gmail.com>
2026-05-05 07:20:37 +0200

    fix: add checks for RGB format and its commas

--- src/parsing/texture_from_line.c
+++ src/parsing/texture_from_line.c
@@ -6,7 +6,7 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 13:54:45 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/04/28 16:08:34 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/05/05 07:14:20 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ static int			rgb_texture_from_line(char *line, t_scene *scene);
 
 static int			read_rgb(char **rgb_split, int *rgb_texture,
 						t_scene *scene);
+
+static int			*read_color(char *color_str, int *color);
 
 int	texture_from_line(char *line, t_scene *scene)
 {
@@ -75,13 +77,13 @@ static int	rgb_texture_from_line(char *line, t_scene *scene)
 		rgb_texture = &scene->textures.c;
 	if (!(*rgb_texture & (255 << 24)))
 		return (!set_scene_log(scene, LOG_DUP_TEXTURE));
-	rgb_split = ft_split_set(line + 1, " \t,");
+	rgb_split = ft_split(line + 1, ',');
 	if (rgb_split == NULL)
 		return (!set_scene_log(scene, LOG_ALLOC_ERR));
 	split_size = 0;
 	while (rgb_split[split_size] != NULL)
 		split_size++;
-	if (split_size != 3)
+	if (split_size != 3 || str_count(line + 1, ',') != 2)
 	{
 		ft_free_arr(rgb_split, free);
 		return (!set_scene_log(scene, LOG_BAD_RGB));
@@ -94,21 +96,34 @@ static int	rgb_texture_from_line(char *line, t_scene *scene)
 static int	read_rgb(char **rgb_split, int *rgb_texture,
 				t_scene *scene)
 {
-	char	*endptr;
 	int		r;
 	int		g;
 	int		b;
 
-	endptr = NULL;
-	r = ft_strtol(rgb_split[0], &endptr, 10);
-	if (endptr == NULL || *endptr != '\0' || r < 0 || r > 255)
-		return (!set_scene_log(scene, LOG_BAD_RGB));
-	g = ft_strtol(rgb_split[1], &endptr, 10);
-	if (endptr == NULL || *endptr != '\0' || g < 0 || g > 255)
-		return (!set_scene_log(scene, LOG_BAD_RGB));
-	b = ft_strtol(rgb_split[2], &endptr, 10);
-	if (endptr == NULL || *endptr != '\0' || b < 0 || b > 255)
+	if (read_color(rgb_split[0], &r) == NULL
+		|| read_color(rgb_split[1], &g) == NULL
+		|| read_color(rgb_split[2], &b) == NULL)
 		return (!set_scene_log(scene, LOG_BAD_RGB));
 	*rgb_texture = (r << 16) + (g << 8) + b;
 	return (1);
+}
+
+static int	*read_color(char *color_str, int *color)
+{
+	char	*endptr;
+
+	color_str = ft_strtrim(color_str, " \t");
+	if (color_str == NULL)
+		return (NULL);
+	endptr = NULL;
+	*color = ft_strtol(color_str, &endptr, 10);
+	if (endptr == NULL || *endptr != '\0')
+	{
+		free(color_str);
+		return (NULL);
+	}
+	free(color_str);
+	if (*color < 0 || *color > 255)
+		return (NULL);
+	return (color);
 }
