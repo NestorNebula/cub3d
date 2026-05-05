e9ec60777b1bfddacef13416d54210d5f469ac24
Noa Houssier <nestornebula.dev@gmail.com>
2026-05-05 07:39:57 +0200

    feat: add check_extension function

--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,11 +6,12 @@
 /*   By: nhoussie <nhoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 07:10:54 by nhoussie          #+#    #+#             */
-/*   Updated: 2026/05/05 07:13:41 by nhoussie         ###   ########.fr       */
+/*   Updated: 2026/05/05 07:31:56 by nhoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <scene.h>
+#include "libft.h"
 
 int			str_count(char *str, char c)
 {
@@ -26,4 +27,19 @@ int			str_count(char *str, char c)
 		str++;
 	}
 	return (i);
+}
+
+int			check_extension(char *filename, char *extension)
+{
+	char	*ext;
+	size_t	extension_len;
+
+	if (filename == NULL || extension == NULL)
+		return (0);
+	extension_len = ft_strlen(extension);
+	ext = ft_strrchr(filename, '.');
+	if (ft_strlen(filename) < extension_len + 1 || ext == NULL
+		|| ft_strncmp(ext, extension, extension_len + 1) != 0 || *(ext - 1) == '/')
+		return (0);
+	return (1);
 }
